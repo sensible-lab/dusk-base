a8bd18246d6ab8cab6ed550a6b5de71af8da879a
Andy To <andyto@gmail.com>
2023-10-29 18:43:22 +0800

    setting for font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,15 @@ module.exports = {
       primary: 'rgb(var(--color-primary) / <alpha-value>)',
       secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
     },
+    fontFamily: {
+      body: 'var(--font-body-family)',
+      heading: 'var(--font-heading-family)',
+    },
+    fontWeight: {
+      body: 'var(--font-body-weight)',
+      bold: 'var(--font-body-weight-bold)',
+      heading: 'var(--font-heading-weight)',
+    },
     extend: {},
   },
   plugins: [],
