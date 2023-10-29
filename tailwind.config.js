7f679ce225362e89f24f46117621df8933e8fa54
Andy To <andyto@gmail.com>
2023-10-29 17:09:51 +0800

    adding settings for color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ['./layout/*.liquid', './templates/*.liquid', './sections/*.liquid', './snippets/*.liquid'],
   theme: {
+    colors: {
+      primary: 'rgb(var(--color-primary) / <alpha-value>)',
+      secondary: 'rgb(var(--color-secondary) / <alpha-value>)',
+    },
     extend: {},
   },
   plugins: [],
