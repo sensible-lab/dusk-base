556bae8a03cce8c723f79f029d862f8973dc1904
samwalker <samwalker505@gmail.com>
2023-10-31 14:20:42 +0800

    feat: add basic footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,16 @@ module.exports = {
       heading: 'var(--font-heading-weight)',
     },
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+      center: true,
+    },
   },
   plugins: [],
 };
