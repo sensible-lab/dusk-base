f1ec61cb3354e7d5ec5ecac0ea3ea0cc0bc95167
Andy To <andyto@gmail.com>
2023-11-08 20:44:08 +0800

    adding price range

--- prettier.config.js
+++ prettier.config.js
@@ -1,7 +1,14 @@
 const config = {
   printWidth: 120,
   singleQuote: true,
-  overrides: [],
+  overrides: [
+    {
+      files: '*.liquid',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
   plugins: ['@shopify/prettier-plugin-liquid', 'prettier-plugin-tailwindcss'],
 };
 
